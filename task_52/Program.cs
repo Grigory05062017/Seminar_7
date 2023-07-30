f7c1937e2cd787b2225aa4a3ec8c6b1a069f1a97
Grigory <free_time_88@mail.ru>
2023-07-30 14:19:02 +0500

    task52

---
+++ task_52/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+void InputArray(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = new Random().Next(0, 10);
+Console.Write($"{matrix[i, j]} \t");
+}
+Console.WriteLine();
+}
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+int i,j;
+Random rand = new Random();
+for (j = 0; j < matrix.GetLength(1); j++)
+{
+double sum = 0;
+for (i = 0; i < matrix.GetLength(0); i++)
+{
+sum = sum + matrix[i,j];
+}
+Console.Write ($"{sum/(i):F1}; \t");
+}
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: "); 
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputArray(matrix);
+ReleaseMatrix(matrix);
